d5018aa865a1454bafda22119f5d9ad2b3122367
Horacio Daniel Ros <auxtral.os@gmail.com>
2026-02-18 23:02:00 -0300

    .

--- include/horizon/application.hpp
+++ include/horizon/application.hpp
@@ -5,11 +5,14 @@ namespace horizon
 
     class Application
     {
-      public:
+    public:
         explicit Application();
         ~Application();
 
         Application(const Application &) = delete;            // Sin soporte para copias.
         Application &operator=(const Application &) = delete; // Sin soporte para asignaciones.
+
+        Application(const Application &&) noexcept; // Puede mover.
+        Application &operator=(const Application &&) noexcept;
     };
 } // namespace horizon
