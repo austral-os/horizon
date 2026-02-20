5a1b5e19c098bb5bea1d66169d5260a959ff863f
Horacio Daniel Ros <auxtral.os@gmail.com>
2026-02-20 19:59:02 -0300

    .

--- include/horizon/GraphicsContext.hpp
+++ include/horizon/GraphicsContext.hpp
@@ -8,10 +8,10 @@ namespace horizon
     public:
         virtual ~GraphicsContext() = default;
 
-        virtual void setColor(float r, float g, float b, float a = 1.0f);
-        virtual void drawRect(int x, int y, int width, int height);
-        virtual void fillRect(int x, int y, int width, int height);
-        virtual void flush();
+        virtual void setColor(float r, float g, float b, float a = 1.0f) {};
+        virtual void drawRect(int x, int y, int width, int height) {};
+        virtual void fillRect(int x, int y, int width, int height) {};
+        virtual void flush() {};
     };
 
 } // namespace horizon
