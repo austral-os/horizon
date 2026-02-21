2894d011ea26b3a59f7b9bc5712ee7e6cd975cb7
Horacio Daniel Ros <auxtral.os@gmail.com>
2026-02-21 01:17:02 -0300

    clean

--- src/core/CairoGraphicsContext.cpp
+++ src/core/CairoGraphicsContext.cpp
@@ -1,24 +1,21 @@
 #include <horizon/CairoGraphicsContext.hpp>
-#include <iostream>
 
 namespace horizon
 {
 
     CairoGraphicContext::CairoGraphicContext(void *data, int w, int h)
     {
-        cairo_s = cairo_image_surface_create_for_data(
-            (unsigned char *)data, CAIRO_FORMAT_ARGB32, w, h, w * 4);
+        cairo_s = cairo_image_surface_create_for_data((unsigned char *)data, CAIRO_FORMAT_ARGB32, w,
+                                                      h, w * 4);
         cr = cairo_create(cairo_s);
     }
 
     CairoGraphicContext::~CairoGraphicContext()
     {
-        std::cout << "haciendo free" << std::endl;
         if (cr)
             cairo_destroy(cr);
         if (cairo_s)
             cairo_surface_destroy(cairo_s);
-        std::cout << "terminando free" << std::endl;
     }
     void CairoGraphicContext::setColor(float r, float g, float b, float a)
     {
