a5f9309ccd8c751f7692337e006c2f7b79abe7ed
Horacio Daniel Ros <auxtral.os@gmail.com>
2026-02-24 14:02:17 -0300

    .

--- src/core/Button.cpp
+++ src/core/Button.cpp
@@ -1,3 +1,4 @@
+#include <cstdio>
 #include <horizon/Application.hpp>
 #include <horizon/Button.hpp>
 #include <horizon/GraphicsContext.hpp>
@@ -24,46 +25,47 @@ namespace horizon
 
     void Button::draw(GraphicsContext &gc)
     {
-        std::cout << "Drawing button" << std::endl;
 
-        Color highlight(0.95f, 0.95f, 0.95f, 1.0f);
+        auto *tm = application()->theme_manager.get();
+        auto font = tm->get_font("window");
+
+        Color window_bg = tm->get_color("primary");
+        Color window_fg = tm->get_color("window_fg");
+        Color shadow_color = tm->get_color("window_border");
+        Color border_color = shadow_color.darker(50.0f);
+
+        Color top1 = window_bg;
+        Color top2 = top1.lighter(50.f);
+        Color bot1 = top2;
+        Color bot2 = window_bg.lighter(10.0f);
+        Color highlight = window_bg;
         Color highlight2 = highlight.with_alpha(50.0f);
+        Color text_color = window_fg;
 
         int radius = m_height / 2;
 
         // Borde exterior adicional (sombra inferior, color más claro que el negro)
-        Color shadowColor(0.4f, 0.4f, 0.4f, 1.0f);
-        gc.setColor(shadowColor);
+        gc.setColor(shadow_color);
         gc.drawRect(m_start_draw_x, m_start_draw_y - 1, m_width, m_height,
                     {radius, radius, radius, radius}, 1.0f);
 
         // Outer border (gray/black)
-        Color borderColor(0.35f, 0.35f, 0.35f, 1.0f);
-        gc.setColor(borderColor);
+        gc.setColor(border_color);
         gc.drawRect(m_start_draw_x, m_start_draw_y, m_width, m_height - 3,
                     {radius, radius, radius, radius}, 1.5f);
 
-        // Top half: gradient from white to very light gray (creates a glass reflection effect)
-        Color top1(0.85f, 0.85f, 0.85f, 1.0f);
-        Color top2(0.8f, 0.8f, 0.8f, 1.0f);
-
         if (is_hovered())
         {
-            std::cout << "Hovered" << std::endl;
-            // top1 = top1.lighter(50.0f);
-            // top2 = top2.lighter(50.0f);
-
             highlight = highlight.lighter(100.0f);
             highlight2 = highlight.with_alpha(50.0f);
         }
 
+        // Top half: gradient from white to very light gray (creates a glass reflection effect)
         int halfHeight = m_height / 2;
         gc.fillLinearGradientRect(m_start_draw_x + 1, m_start_draw_y + 1, m_width - 2, halfHeight,
                                   top1, top2, true, {radius - 1, radius - 1, 0, 0});
 
         // Bottom half: gradient starting slightly darker and lightening towards the bottom
-        Color bot1(0.8f, 0.8f, 0.8f, 1.0f);
-        Color bot2(0.97f, 0.97f, 0.97f, 1.0f);
         gc.fillLinearGradientRect(m_start_draw_x + 1, m_start_draw_y + halfHeight, m_width - 2,
                                   m_height - 4 - halfHeight, bot1, bot2, true,
                                   {0, 0, radius - 1, radius - 1});
@@ -75,21 +77,22 @@ namespace horizon
         int h_radius_top = radius - 2;       // El radio superior se adapta al botón
         int h_radius_bot = h_radius_top / 2; // El radio inferior es mucho más curvo/chico
 
-        gc.fillLinearGradientRect(m_start_draw_x + h_margin_x, m_start_draw_y + 2, h_width,
-                                  h_height, highlight, // Blanco sólido arriba
-                                  highlight2,          // Blanco casi transparente abajo
-                                  true, {h_radius_top, h_radius_top, h_radius_bot, h_radius_bot});
-
+        /* gc.fillLinearGradientRect(m_start_draw_x + h_margin_x, m_start_draw_y + 2, h_width,
+                                       h_height, highlight, // Blanco sólido arriba
+                                       highlight2,          // Blanco casi transparente abajo
+                                       true,
+                                       {h_radius_top, h_radius_top, h_radius_bot, h_radius_bot});
+        */
         // Center the text
-        TextMetrics metrics = gc.getTextMetrics(m_text.c_str(), "Lucida Grande", 20,
+        TextMetrics metrics = gc.getTextMetrics(m_text.c_str(), font.family.c_str(), font.size,
                                                 FONT_SLANT_NORMAL, FONT_WEIGHT_NORMAL);
 
         int text_x = m_start_draw_x + (m_width / 2) - (metrics.width / 2);
         int text_y = m_start_draw_y + (m_height / 2) + (metrics.height / 2) - 3;
 
         // Draw the text
-        gc.setDrawFont("Lucida Grande", 20, FONT_SLANT_NORMAL, FONT_WEIGHT_NORMAL);
-        gc.setColor(Color(0.1f, 0.1f, 0.1f, 1.0f));
+        gc.setDrawFont(font.family.c_str(), font.size, FONT_SLANT_NORMAL, FONT_WEIGHT_NORMAL);
+        gc.setColor(text_color);
         gc.drawText(text_x, text_y, m_text.c_str());
     }
 
