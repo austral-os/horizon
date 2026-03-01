3c9b5d30dbc2e898a95d46b757612a35010a6473
Horacio Daniel Ros <auxtral.os@gmail.com>
2026-02-28 23:17:21 -0300

    ahora solid object se dibuja de distintas maneras de acuerdo al draw state

--- src/core/SolidObject.cpp
+++ src/core/SolidObject.cpp
@@ -14,12 +14,30 @@ namespace horizon
             [this](EventContext &)
             {
                 // Redibuja el widget
+                set_draw_state(get_draw_state(WidgetEvent::MOUSE_ENTER));
                 invalidate();
             });
         when_mouse_leave.connect(
             [this](EventContext &)
             {
                 // Redibuja el widget
+                set_draw_state(get_draw_state(WidgetEvent::MOUSE_LEAVE));
+                invalidate();
+            });
+
+        when_mouse_press.connect(
+            [this](EventContext &)
+            {
+                // Redibuja el widget
+                set_draw_state(get_draw_state(WidgetEvent::MOUSE_PRESS));
+                invalidate();
+            });
+
+        when_mouse_release.connect(
+            [this](EventContext &)
+            {
+                // Redibuja el widget
+                set_draw_state(get_draw_state(WidgetEvent::MOUSE_RELEASE));
                 invalidate();
             });
     }
@@ -72,42 +90,116 @@ namespace horizon
             break;
         }
 
-        Color top1 = c1;
-
-        Color highlight = window_bg;
-        Color highlight2 = highlight.with_alpha(0.3f);
-        Color text_color = window_fg;
-
-        // clear background
-        gc.setColor(window_bg);
-        gc.fillRect(m_x, m_y, m_width, m_height, m_corner_radius);
-
-        // Outer border (gray/black)
-        gc.setColor(border_color);
-        gc.drawRect(m_start_draw_x, m_start_draw_y, m_width, m_height - 3, m_corner_radius, 1.0f);
-
-        if (is_hovered())
+        if (m_draw_state == WidgetDrawState::NORMAL)
         {
+
+            Color top1 = c1;
+
+            Color highlight = window_bg;
+            Color highlight2 = highlight.with_alpha(0.3f);
+            Color text_color = window_fg;
+
+            // clear background
+            gc.setColor(window_bg);
+            gc.fillRect(m_x, m_y, m_width, m_height, m_corner_radius);
+
+            // Outer border (gray/black)
+            gc.setColor(border_color);
+            gc.drawRect(m_start_draw_x, m_start_draw_y, m_width, m_height - 3, m_corner_radius,
+                        1.0f);
+
+            // Top half: gradient from white to very light gray (creates a glass reflection effect)
+            int halfHeight = m_height / 2;
+            gc.fillLinearGradientRect(m_start_draw_x + 1, m_start_draw_y + 1, m_width - 2,
+                                      halfHeight, highlight.darker(2.f), highlight.darker(10.f),
+                                      true,
+                                      {m_corner_radius.top_left, m_corner_radius.top_right, 0, 0});
+
+            // Bottom half: gradient starting slightly darker and lightening towards the bottom
+            gc.fillLinearGradientRect(
+                m_start_draw_x + 1, m_start_draw_y + halfHeight, m_width - 2,
+                m_height - 4 - halfHeight, top1.darker(10.f), top1.darker(10.f), true,
+                {0, 0, m_corner_radius.bottom_right, m_corner_radius.bottom_left});
+
+            // Borde inferior
+            gc.setColor(border_color.lighter(80.f));
+            gc.fillRect(m_x + m_corner_radius.bottom_left,
+                        m_start_draw_y + m_available_draw_height - 2,
+                        m_width - m_corner_radius.bottom_left - m_corner_radius.bottom_right, 2);
+        }
+        else if (m_draw_state == WidgetDrawState::HOVERED)
+        {
+            Color top1 = c1;
+
+            Color highlight = window_bg;
+            Color highlight2 = highlight.with_alpha(0.3f);
+            Color text_color = window_fg;
+
+            // clear background
+            gc.setColor(window_bg);
+            gc.fillRect(m_x, m_y, m_width, m_height, m_corner_radius);
+
+            // Outer border (gray/black)
+            gc.setColor(border_color);
+            gc.drawRect(m_start_draw_x, m_start_draw_y, m_width, m_height - 3, m_corner_radius,
+                        1.0f);
+
             highlight = highlight.lighter(100.0f);
             highlight2 = highlight.with_alpha(0.5f);
+
+            // Top half: gradient from white to very light gray (creates a glass reflection effect)
+            int halfHeight = m_height / 2;
+            gc.fillLinearGradientRect(m_start_draw_x + 1, m_start_draw_y + 1, m_width - 2,
+                                      halfHeight, highlight.darker(2.f), highlight.darker(10.f),
+                                      true,
+                                      {m_corner_radius.top_left, m_corner_radius.top_right, 0, 0});
+
+            // Bottom half: gradient starting slightly darker and lightening towards the bottom
+            gc.fillLinearGradientRect(
+                m_start_draw_x + 1, m_start_draw_y + halfHeight, m_width - 2,
+                m_height - 4 - halfHeight, top1.darker(10.f), top1.darker(10.f), true,
+                {0, 0, m_corner_radius.bottom_right, m_corner_radius.bottom_left});
+
+            // Borde inferior
+            gc.setColor(border_color.lighter(80.f));
+            gc.fillRect(m_x + m_corner_radius.bottom_left,
+                        m_start_draw_y + m_available_draw_height - 2,
+                        m_width - m_corner_radius.bottom_left - m_corner_radius.bottom_right, 2);
         }
+        else if (m_draw_state == WidgetDrawState::PRESSED)
+        {
+            Color top1 = c1;
 
-        // Top half: gradient from white to very light gray (creates a glass reflection effect)
-        int halfHeight = m_height / 2;
-        gc.fillLinearGradientRect(m_start_draw_x + 1, m_start_draw_y + 1, m_width - 2, halfHeight,
-                                  highlight.darker(2.f), highlight.darker(10.f), true,
-                                  {m_corner_radius.top_left, m_corner_radius.top_right, 0, 0});
+            Color highlight = window_bg;
+            Color highlight2 = highlight.with_alpha(0.3f);
+            Color text_color = window_fg;
 
-        // Bottom half: gradient starting slightly darker and lightening towards the bottom
-        gc.fillLinearGradientRect(
-            m_start_draw_x + 1, m_start_draw_y + halfHeight, m_width - 2, m_height - 4 - halfHeight,
-            top1.darker(10.f), top1.darker(10.f), true,
-            {0, 0, m_corner_radius.bottom_right, m_corner_radius.bottom_left});
+            // clear background
+            gc.setColor(window_bg);
+            gc.fillRect(m_x, m_y, m_width, m_height, m_corner_radius);
 
-        // Borde inferior
-        gc.setColor(border_color.lighter(80.f));
-        gc.fillRect(m_x + m_corner_radius.bottom_left, m_start_draw_y + m_available_draw_height - 2,
-                    m_width - m_corner_radius.bottom_left - m_corner_radius.bottom_right, 2);
+            // Outer border (gray/black)
+            gc.setColor(border_color);
+            gc.drawRect(m_start_draw_x, m_start_draw_y, m_width, m_height - 3, m_corner_radius,
+                        1.0f);
+
+            highlight = highlight.darker(60.0f);
+            highlight2 = highlight.with_alpha(0.5f);
+
+            // Top half: gradient from white to very light gray (creates a glass reflection effect)
+            int halfHeight = m_height / 2;
+            gc.fillLinearGradientRect(m_start_draw_x + 1, m_start_draw_y + 1, m_width - 2,
+                                      m_height - 4, highlight, highlight.with_alpha(0.1f), true,
+                                      {m_corner_radius.top_left, m_corner_radius.top_right,
+                                       m_corner_radius.bottom_right, m_corner_radius.bottom_left});
+
+            // Borde inferior
+            gc.drawLinearGradientRect(m_start_draw_x + 2, m_start_draw_y + 2, m_width - 2,
+                                      m_height - 3, border_color, border_color.with_alpha(0.f),
+                                      3.0f, true,
+                                      {m_corner_radius.top_left, m_corner_radius.top_right,
+                                       m_corner_radius.bottom_right, m_corner_radius.bottom_left});
+        }
     }
 
 } // namespace horizon
