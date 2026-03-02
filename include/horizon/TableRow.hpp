8c9fdc63c6b22c25befcc6181fd7152e09e38463
Horacio Daniel Ros <auxtral.os@gmail.com>
2026-03-02 14:21:35 -0300

    feat: TableRow now propagates text color to its child Label widgets based on its selection state, utilizing new theme foreground colors.

--- include/horizon/TableRow.hpp
+++ include/horizon/TableRow.hpp
@@ -3,6 +3,7 @@
 #include <horizon/Application.hpp>
 #include <horizon/Color.hpp>
 #include <horizon/GraphicsContext.hpp>
+#include <horizon/Label.hpp>
 #include <horizon/ThemeManager.hpp>
 #include <horizon/Widget.hpp>
 
@@ -40,21 +41,51 @@ namespace horizon
         {
             auto *tm = application()->theme_manager.get();
             Color bg;
+            Color fg;
 
             if (m_is_selected)
+            {
                 bg = tm->get_color("table_row_selected");
+                fg = tm->get_color("table_row_selected_fg");
+            }
             else if (m_is_alternate)
+            {
                 bg = tm->get_color("table_row_alternate");
+                fg = tm->get_color("table_row_fg");
+            }
             else
+            {
                 bg = tm->get_color("table_row");
+                fg = tm->get_color("table_row_fg");
+            }
 
             gc.setColor(bg);
             gc.fillRect(m_x, m_y, m_width, m_height);
+
+            // Propagate text color to children (mostly Labels)
+            apply_text_color_recursive(this, fg);
 
             Widget::draw(gc);
         }
 
     private:
+        void apply_text_color_recursive(Widget *w, Color fg)
+        {
+            if (!w)
+                return;
+
+            // Using dynamic_cast to find Labels and set their color
+            if (auto *lbl = dynamic_cast<Label *>(w))
+            {
+                lbl->set_text_color(fg);
+            }
+
+            for (auto &child : w->children())
+            {
+                apply_text_color_recursive(child.get(), fg);
+            }
+        }
+
         bool m_is_alternate{false};
         bool m_is_selected{false};
     };
