8c89455cc010ead63d01721b5e350ed4458926ea
Horacio Daniel Ros <horaciodrs@gmail.com>
2026-03-14 11:42:07 -0300

    feat: Update multiwindow example to demonstrate an input dialog with data transfer to the main window.

--- examples/multiwindow/main.cpp
+++ examples/multiwindow/main.cpp
@@ -1,9 +1,10 @@
 #include <horizon/Application.hpp>
-#include <horizon/Window.hpp>
+#include <horizon/AquaObject.hpp>
 #include <horizon/Button.hpp>
 #include <horizon/Label.hpp>
-#include <horizon/AquaObject.hpp>
 #include <horizon/Logger.hpp>
+#include <horizon/TextBox.hpp>
+#include <horizon/Window.hpp>
 #include <memory>
 
 using namespace horizon;
@@ -17,34 +18,65 @@ int main(int argc, char *argv[])
 
     // 2. Create the main window content
     auto mainWindow = std::make_unique<Window>("Main Window");
-    
+
     auto container = std::make_unique<Widget>();
     container->set_margin(20);
     container->set_spacing(10);
-    
-    auto label = std::make_unique<Label>("Click the button to open a new window");
-    container->add_child(std::move(label));
-    
+
+    // We keep a raw pointer to the label to update it later
+    auto labelPtr = std::make_unique<Label>("No data received yet");
+    Label *statusLabel = labelPtr.get();
+    container->add_child(std::move(labelPtr));
+
     auto btn = std::make_unique<Button<AquaObject>>();
-    btn->set_text("Open New Window");
+    btn->set_text("Open Input Dialog");
     btn->set_fixed_size(40);
-    
-    btn->when_mouse_press.connect([&app](EventContext &) {
-        LOG_INFO << "Button clicked! Creating new window...";
-        WaylandWindow* newWin = app.create_window(400, 300);
-        newWin->set_name("Dynamic Window");
-        
-        auto content = std::make_unique<Window>("New Dynamic Window");
-        auto lbl = std::make_unique<Label>("I was created dynamically!");
-        lbl->set_margin(20);
-        content->add_child(std::move(lbl));
-        
-        newWin->set_root(std::move(content));
-    });
-    
+
+    btn->when_mouse_press.connect(
+        [&app, statusLabel](EventContext &)
+        {
+            LOG_INFO << "Opening input dialog...";
+            WaylandWindow *dialog = app.create_dialog(nullptr, 400, 300);
+            dialog->set_name("Input Dialog");
+
+            auto content = std::make_unique<Window>("Enter Text");
+            content->set_spacing(15);
+
+            auto container = std::make_unique<Widget>();
+            container->set_margin(20);
+            container->set_spacing(10);
+
+            auto box = std::make_unique<TextBox<TextPolicy>>();
+            box->set_placeholder("Type something here...");
+            box->set_fixed_size(40);
+            TextBox<TextPolicy> *textBoxPtr = box.get();
+            container->add_child(std::move(box));
+
+            auto acceptBtn = std::make_unique<Button<AquaObject>>();
+            acceptBtn->set_text("Accept");
+            acceptBtn->set_fixed_size(40);
+
+            acceptBtn->when_mouse_press.connect(
+                [statusLabel, textBoxPtr, dialog](EventContext &)
+                {
+                    std::string val = textBoxPtr->text();
+                    LOG_INFO << "Sending data to main window: " << val;
+
+                    // Update the label in the main window
+                    statusLabel->set_text("Received: " + val);
+
+                    // Close this dialog
+                    dialog->quit();
+                });
+
+            container->add_child(std::move(acceptBtn));
+            content->add_child(std::move(container));
+            dialog->set_root(std::move(content));
+        });
+
     container->add_child(std::move(btn));
     mainWindow->add_child(std::move(container));
-    
+
     app.set_root(std::move(mainWindow));
 
     // 4. Run the application
