680ce1a43357203f52f0152a9879e74281b5fb34
Horacio Daniel Ros <horaciodrs@gmail.com>
2026-04-15 17:28:56 -0300

    feat: set fixed sizes for toolbar button groups in TextEditorToolbar

--- apps/text-editor/src/TextEditorToolbar.cpp
+++ apps/text-editor/src/TextEditorToolbar.cpp
@@ -4,87 +4,106 @@
 #include <horizon/Spacer.hpp>
 #include <memory>
 
-namespace horizon {
-namespace text_editor {
+namespace horizon
+{
+    namespace text_editor
+    {
 
-TextEditorToolbar::TextEditorToolbar() : Widget() {
-    set_layout_type(WIDGET_LAYOUT_HORIZONTAL);
-    set_position_type(WidgetPositionTypes::FILL);
-    set_margin(5);
-    set_spacing(10);
+        TextEditorToolbar::TextEditorToolbar() : Widget()
+        {
+            set_layout_type(WIDGET_LAYOUT_HORIZONTAL);
+            set_position_type(WidgetPositionTypes::FILL);
+            set_margin(5);
+            set_spacing(10);
 
-    // 1. File Actions group
-    auto file_group = std::make_unique<horizon::GroupButton>();
-    m_file_group = file_group.get();
-    
-    auto new_icon = std::make_unique<horizon::Icon>();
-    new_icon->set_icon_name("document-new");
-    new_icon->set_icon_size(16);
-    m_file_group->add_item(std::move(new_icon));
+            // 1. File Actions group
+            auto file_group = std::make_unique<horizon::GroupButton>();
+            m_file_group = file_group.get();
+            m_file_group->set_fixed_size(150);
 
-    auto open_icon = std::make_unique<horizon::Icon>();
-    open_icon->set_icon_name("document-open");
-    open_icon->set_icon_size(16);
-    m_file_group->add_item(std::move(open_icon));
+            auto new_icon = std::make_unique<horizon::Icon>();
+            new_icon->set_icon_name("document-new");
+            new_icon->set_icon_size(16);
+            m_file_group->add_item(std::move(new_icon));
 
-    auto save_icon = std::make_unique<horizon::Icon>();
-    save_icon->set_icon_name("document-save");
-    save_icon->set_icon_size(16);
-    m_file_group->add_item(std::move(save_icon));
+            auto open_icon = std::make_unique<horizon::Icon>();
+            open_icon->set_icon_name("document-open");
+            open_icon->set_icon_size(16);
+            m_file_group->add_item(std::move(open_icon));
 
-    m_file_group->when_button_clicked.connect([this](horizon::GroupButtonClickEvent& ctx) {
-        EventContext dummy;
-        if (ctx.button_index == 0) when_new_clicked.run(dummy);
-        else if (ctx.button_index == 1) when_open_clicked.run(dummy);
-        else if (ctx.button_index == 2) when_save_clicked.run(dummy);
-    });
+            auto save_icon = std::make_unique<horizon::Icon>();
+            save_icon->set_icon_name("document-save");
+            save_icon->set_icon_size(16);
+            m_file_group->add_item(std::move(save_icon));
 
-    // 2. Edit Actions group
-    auto edit_group = std::make_unique<horizon::GroupButton>();
-    m_edit_group = edit_group.get();
+            m_file_group->when_button_clicked.connect(
+                [this](horizon::GroupButtonClickEvent &ctx)
+                {
+                    EventContext dummy;
+                    if (ctx.button_index == 0)
+                        when_new_clicked.run(dummy);
+                    else if (ctx.button_index == 1)
+                        when_open_clicked.run(dummy);
+                    else if (ctx.button_index == 2)
+                        when_save_clicked.run(dummy);
+                });
 
-    auto undo_icon = std::make_unique<horizon::Icon>();
-    undo_icon->set_icon_name("edit-undo");
-    undo_icon->set_icon_size(16);
-    m_edit_group->add_item(std::move(undo_icon));
+            // 2. Edit Actions group
+            auto edit_group = std::make_unique<horizon::GroupButton>();
+            m_edit_group = edit_group.get();
+            m_edit_group->set_fixed_size(100);
 
-    auto redo_icon = std::make_unique<horizon::Icon>();
-    redo_icon->set_icon_name("edit-redo");
-    redo_icon->set_icon_size(16);
-    m_edit_group->add_item(std::move(redo_icon));
+            auto undo_icon = std::make_unique<horizon::Icon>();
+            undo_icon->set_icon_name("edit-undo");
+            undo_icon->set_icon_size(16);
+            m_edit_group->add_item(std::move(undo_icon));
 
-    m_edit_group->when_button_clicked.connect([this](horizon::GroupButtonClickEvent& ctx) {
-        EventContext dummy;
-        if (ctx.button_index == 0) when_undo_clicked.run(dummy);
-        else if (ctx.button_index == 1) when_redo_clicked.run(dummy);
-    });
+            auto redo_icon = std::make_unique<horizon::Icon>();
+            redo_icon->set_icon_name("edit-redo");
+            redo_icon->set_icon_size(16);
+            m_edit_group->add_item(std::move(redo_icon));
 
-    add_child(std::move(file_group));
-    add_child(std::move(edit_group));
-    add_child(horizon::Spacer()); 
+            m_edit_group->when_button_clicked.connect(
+                [this](horizon::GroupButtonClickEvent &ctx)
+                {
+                    EventContext dummy;
+                    if (ctx.button_index == 0)
+                        when_undo_clicked.run(dummy);
+                    else if (ctx.button_index == 1)
+                        when_redo_clicked.run(dummy);
+                });
 
-    // 3. Settings Actions group (Right)
-    auto settings_group = std::make_unique<horizon::GroupButton>();
-    m_settings_group = settings_group.get();
+            add_child(std::move(file_group));
+            add_child(std::move(edit_group));
+            add_child(horizon::Spacer());
 
-    auto fullscreen_icon = std::make_unique<horizon::Icon>();
-    fullscreen_icon->set_icon_name("view-fullscreen");
-    fullscreen_icon->set_icon_size(16);
-    m_settings_group->add_item(std::move(fullscreen_icon));
+            // 3. Settings Actions group (Right)
+            auto settings_group = std::make_unique<horizon::GroupButton>();
+            m_settings_group = settings_group.get();
+            m_settings_group->set_fixed_size(100);
 
-    auto preferences_icon = std::make_unique<horizon::Icon>();
-    preferences_icon->set_icon_name("emblem-system");
-    preferences_icon->set_icon_size(16);
-    m_settings_group->add_item(std::move(preferences_icon));
+            auto fullscreen_icon = std::make_unique<horizon::Icon>();
+            fullscreen_icon->set_icon_name("view-fullscreen");
+            fullscreen_icon->set_icon_size(16);
+            m_settings_group->add_item(std::move(fullscreen_icon));
 
-    m_settings_group->when_button_clicked.connect([this](horizon::GroupButtonClickEvent& ctx) {
-        EventContext dummy;
-        if (ctx.button_index == 0) when_fullscreen_clicked.run(dummy);
-        else if (ctx.button_index == 1) when_preferences_clicked.run(dummy);
-    });
+            auto preferences_icon = std::make_unique<horizon::Icon>();
+            preferences_icon->set_icon_name("emblem-system");
+            preferences_icon->set_icon_size(16);
+            m_settings_group->add_item(std::move(preferences_icon));
 
-    add_child(std::move(settings_group));
-}
+            m_settings_group->when_button_clicked.connect(
+                [this](horizon::GroupButtonClickEvent &ctx)
+                {
+                    EventContext dummy;
+                    if (ctx.button_index == 0)
+                        when_fullscreen_clicked.run(dummy);
+                    else if (ctx.button_index == 1)
+                        when_preferences_clicked.run(dummy);
+                });
 
-} // namespace text_editor
+            add_child(std::move(settings_group));
+        }
+
+    } // namespace text_editor
 } // namespace horizon
