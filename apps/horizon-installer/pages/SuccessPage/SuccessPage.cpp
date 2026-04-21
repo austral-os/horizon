3d8d4a07ac7814513d02cbaddbc50c9d61ae4abc
Horacio Daniel Ros <auxtral.os@gmail.com>
2026-04-21 19:42:41 -0300

    refactor: reorder includes and improve code formatting and layout in SuccessPage

--- apps/horizon-installer/pages/SuccessPage/SuccessPage.cpp
+++ apps/horizon-installer/pages/SuccessPage/SuccessPage.cpp
@@ -1,8 +1,8 @@
 #include "SuccessPage.hpp"
 #include <horizon/I18n.hpp>
-#include <horizon/Spacer.hpp>
-#include <horizon/Label.hpp>
 #include <horizon/Icon.hpp>
+#include <horizon/Label.hpp>
+#include <horizon/Spacer.hpp>
 #include <horizon/ToolbarButton.hpp>
 
 namespace horizon::installer
@@ -18,7 +18,7 @@ namespace horizon::installer
         logo->set_icon_name("software-installed-symbolic");
         logo->set_icon_size(128);
         logo->set_size(128, 128);
-        
+
         auto logo_container = std::make_unique<Widget>();
         logo_container->set_layout_type(WIDGET_LAYOUT_HORIZONTAL);
         logo_container->add_child(Spacer());
@@ -39,17 +39,21 @@ namespace horizon::installer
 
         add_child(Spacer());
 
-        auto btn_finish = std::make_unique<ToolbarButton>(i18n().tr("installer.buttons.restart"), "system-reboot");
-        btn_finish->when_click.connect([this](auto&) { 
-            EventContext ctx;
-            when_finish.run(ctx); 
-        });
-        
+        auto btn_finish = std::make_unique<ToolbarButton>(i18n().tr("installer.buttons.restart"),
+                                                          "system-reboot");
+        btn_finish->when_click.connect(
+            [this](auto &)
+            {
+                EventContext ctx;
+                when_finish.run(ctx);
+            });
+
         auto btn_container = std::make_unique<Widget>();
         btn_container->set_layout_type(WIDGET_LAYOUT_HORIZONTAL);
         btn_container->add_child(Spacer());
         btn_container->add_child(std::move(btn_finish));
         btn_container->add_child(Spacer());
+        btn_container->set_fixed_size(70);
         add_child(std::move(btn_container));
     }
 } // namespace horizon::installer
