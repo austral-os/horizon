02b971d88d8b05f6e9e1f3bac5b8222410e39e96
Horacio Daniel Ros <auxtral.os@gmail.com>
2026-04-25 11:05:45 -0300

    feat: localize application titles and metadata in document and image viewers

--- apps/document-viewer/src/main.cpp
+++ apps/document-viewer/src/main.cpp
@@ -10,15 +10,20 @@ int main(int argc, char **argv)
     horizon::I18n::set_search_paths({"/home/horacio/Desarrollo/austral-os/horizon", ".", ".."});
     horizon::i18n().load_app_locales("document-viewer");
 
+    app.set_name(horizon::i18n().tr("app.title"));
+    app.set_icon_name("document-viewer");
+
     // Setup About info
     auto &about = app.about_manager();
-    about.set_app_title("Document Viewer");
+    about.set_app_title(horizon::i18n().tr("app.title"));
     about.set_app_description("A simple and powerful document viewer for Horizon.");
     about.set_app_version("0.1.0");
     about.set_app_icon("document-viewer");
     about.set_app_git(horizon::ABOUT_HORIZON.git);
-    about.add_app_author("Horacio Daniel Ros", "https://github.com/austral-os/horizon", "horaciodrs@gmail.com");
-    about.add_app_translator("Horacio Daniel Ros", "https://github.com/austral-os/horizon", "horaciodrs@gmail.com");
+    about.add_app_author("Horacio Daniel Ros", "https://github.com/austral-os/horizon",
+                         "horaciodrs@gmail.com");
+    about.add_app_translator("Horacio Daniel Ros", "https://github.com/austral-os/horizon",
+                             "horaciodrs@gmail.com");
 
     auto window = std::make_unique<horizon::pdf::DocumentWindow>();
 
--- apps/image-viewer/src/main.cpp
+++ apps/image-viewer/src/main.cpp
@@ -2,29 +2,37 @@
 #include <horizon/Application.hpp>
 #include <horizon/I18n.hpp>
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv)
+{
     horizon::Application app("image-viewer", 1024, 768);
     horizon::i18n().load_app_locales("image-viewer");
-    
+
+    app.set_name(horizon::i18n().tr("app.title"));
+    app.set_icon_name("image-viewer");
+
     // Setup About info
     auto &about = app.about_manager();
-    about.set_app_title("Image Viewer");
+    about.set_app_title(horizon::i18n().tr("app.title"));
     about.set_app_description("A fast and versatile image viewer for Horizon.");
     about.set_app_version("0.1.0");
     about.set_app_icon("image-viewer");
     about.set_app_git(horizon::ABOUT_HORIZON.git);
-    about.add_app_author("Horacio Daniel Ros", "https://github.com/austral-os/horizon", "horaciodrs@gmail.com");
-    about.add_app_translator("Horacio Daniel Ros", "https://github.com/austral-os/horizon", "horaciodrs@gmail.com");
-    
+    about.add_app_author("Horacio Daniel Ros", "https://github.com/austral-os/horizon",
+                         "horaciodrs@gmail.com");
+    about.add_app_translator("Horacio Daniel Ros", "https://github.com/austral-os/horizon",
+                             "horaciodrs@gmail.com");
+
     auto window = std::make_unique<horizon::image::ImageViewerWindow>();
-    
-    if (argc > 1) {
+
+    if (argc > 1)
+    {
         // Abrir cada archivo pasado como argumento en una pestaña
-        for (int i = 1; i < argc; ++i) {
+        for (int i = 1; i < argc; ++i)
+        {
             window->open_file(argv[i]);
         }
     }
-    
+
     app.set_root(std::move(window));
     app.run();
     return 0;
