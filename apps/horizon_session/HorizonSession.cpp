85b3f2eed0c4c5482e022e8b5ae1d5fb39b7395a
Horacio Daniel Ros <auxtral.os@gmail.com>
2026-06-01 15:55:52 -0300

    feat: add ArkFM configuration file to Horizon session and increment ArkFM version

--- apps/horizon_session/HorizonSession.cpp
+++ apps/horizon_session/HorizonSession.cpp
@@ -265,7 +265,7 @@ void HorizonSession::init(const std::string &compositor)
                                                  "power.json",   "text-editor.json",
                                                  "nova.json",    "capture.json",
                                                  "mouse.json",   "notifications.json",
-                                                 "color-scheme.json"};
+                                                 "color-scheme.json", "arkfm.json"};
 
         for (const auto &config_file : config_files)
         {
