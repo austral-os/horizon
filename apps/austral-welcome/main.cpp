94343579eb233565af13c8451ce2a8d0458683a0
Horacio Daniel Ros <auxtral.os@gmail.com>
2026-06-06 21:49:51 -0300

    fix: austral-welcome icon

--- apps/austral-welcome/main.cpp
+++ apps/austral-welcome/main.cpp
@@ -13,14 +13,14 @@ int main(int argc, char *argv[])
     horizon::i18n().load_app_locales("austral-welcome");
 
     app->set_name(horizon::i18n().tr("welcome.title"));
-    app->set_icon_name("emblem-austral");
+    app->set_icon_name("austral-welcome");
 
     // Setup About info
     auto &about = app->about_manager();
     about.set_app_title(horizon::i18n().tr("welcome.title"));
     about.set_app_description(horizon::i18n().tr("welcome.description"));
     about.set_app_version(APP_VERSION);
-    about.set_app_icon("emblem-austral");
+    about.set_app_icon("austral-welcome");
 
     auto window = std::make_unique<WelcomeWindow>();
 
